94646030e705a0f4cb5afb42080f0aade86babff
Leonardo Lee <lee.leonardo.king@gmail.com>
2014-09-08 14:23:38 -0700

    Added the locationManager stuff, I am going to have to port it over to the MKEngine built by Kirby at a later point.

--- DrunkardsWalk/VisitNotifications.swift
+++ DrunkardsWalk/VisitNotifications.swift
@@ -8,17 +8,49 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class VisitNotifications {
+class VisitNotifications: NSObject, CLLocationManagerDelegate {
     var runName : String
     var locations : [CLLocation]?
     
+    var fakeRegionManager : CLLocationManager
+    
     init(name runCalled: String){
         self.runName = runCalled
+        self.fakeRegionManager = CLLocationManager()
 
     }
     
-    class func generateNotifications() {
+    func startMonitoringRegions() {
+        if CLLocationManager.locationServicesEnabled() {
+            
+        } else {
+            fakeRegionManager.requestAlwaysAuthorization()
+        }
+    }
+    
+    /*
+    This method is called by the MapSearchEngine's delegate searchResults method.
+    */
+    class func generateNotifications(searchResult: [MKMapItem]) {
+        
+        for mapItem in searchResult {
+            if mapItem.name != "Current Location" {
+                var coordinate = mapItem.placemark.coordinate
+                var region = CLCircularRegion(circularRegionWithCenter: coordinate, radius: 25, identifier: "PubRegions")
+                
+                //This should present a notification to changes to the CLCircularRegion on exit.
+                region.notifyOnEntry = true //As of right now lets let this stay on.
+                region.notifyOnExit = true
+                
+                //Monitor regions from here.
+                //self.fakeRegionManager.startMonitoringRegions(region)
+            }
+        }
+        
+        //This notification stuff is going to be manipulated and changed about for a while.
+        //This will probably set to a notification that is based not on the region, but rather just called.
         var notification = UILocalNotification()
         notification.soundName = UILocalNotificationDefaultSoundName
         //notification.applicationIconBadgeNumber
@@ -47,6 +79,15 @@ class VisitNotifications {
         //An array of location of MKMapItems -> CLLocation
         //These CLLocations are going to be used to set the regions that we will be using.
         
+    }
+    
+    //MARK: CLLocationManager
+    func locationManager(manager: CLLocationManager!, didEnterRegion region: CLRegion!) {
+        println("Entered region: \(region)")
+    }
+    
+    func locationManager(manager: CLLocationManager!, didExitRegion region: CLRegion!) {
+        println("Exited region: \(region)")
     }
 }
 
