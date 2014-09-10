06d04b4c80fa2a6678371cdf19d101201ff28354
Leonardo Lee <lee.leonardo.king@gmail.com>
2014-09-10 14:37:24 -0700

    Notification improvements. 1. Implemented the Constants for Notification. 2. Made AppDelegate a bit cleaner, started to add code to work with Notification related region monitoring. I need the regions, Uber related code and such before I can add the stuff. 3. Finished the notification actions and the setup for them.

---
+++ DrunkardsWalk/Constants.swift
@@ -0,0 +1,32 @@
+//
+//  Constants.swift
+//  DrunkardsWalk
+//
+//  Created by Leonardo Lee on 9/10/14.
+//  Copyright (c) 2014 CCA. All rights reserved.
+//
+
+import Foundation
+
+//MARK: - Notification Constants
+//let kNotificationCategoryPubCrawl = "PUB_CAT"
+//let kNotificationCategoryUber = "UBER_CAT"
+//let kNotificationActionUber = "UBER_ID"
+//let kNotificationActionCheck = "CHECK_ID"
+//let kNotificationActionCancel = "CANCEL_ID"
+//let kNotificationActionRateUp = "RATEUP_ID"
+//let kNotificationActionRateDown = "RATEDOWN_ID"
+
+struct kNotification {
+    enum Action : String {
+        case Check = "CHECK_ID"
+        case Cancel = "CANCEL_ID"
+        case RateUp = "RATEUP_ID"
+        case RateDown = "RATEDOWN_ID"
+        case CallUber = "UBER_CALL_ID"
+    }
+    enum Category : String {
+        case PubCrawl = "PUB_CAT"
+        case Uber = "UBER_CAT"
+    }
+}
--- DrunkardsWalk/NotificationController.swift
+++ DrunkardsWalk/NotificationController.swift
@@ -13,11 +13,16 @@
 import UIKit
 import MapKit
 
+
+//We still need to do the logic for Uber. Finesse a bit of logic to help the use not spend money on getting to a loction that they can easily walk to. We'll have to figure out a good place to get that working.
+
 class NotificationController {
     
-    init(){}
+    init(){
+        self.setupNotifications()
+    }
     
-    //MARK: Scheduling a Notification
+//MARK: - Scheduling a Notification
     func scheduleNotificationWithRegion(region : CLRegion) {
         
         /*
@@ -59,101 +64,56 @@ class NotificationController {
     }
     
     //MARK: - Notification Actions
-    func setupNotificationActions() {
-        //Sample notification:
-        var acceptAction = UIMutableUserNotificationAction()
-        acceptAction.title = "Accept"
-        acceptAction.identifier = "ACCEPT_ID"
-        acceptAction.activationMode = UIUserNotificationActivationMode.Background
-        
-        var trashAction = UIMutableUserNotificationAction()
-        trashAction.title = "Trash"
-        trashAction.identifier = "TRASH_ID"
-        trashAction.activationMode = UIUserNotificationActivationMode.Background
-        
-        var replyAction = UIMutableUserNotificationAction()
-        replyAction.title = "Reply"
-        replyAction.identifier = "REPLY_ID"
-        replyAction.activationMode = UIUserNotificationActivationMode.Foreground
-        
-        var registeredActions = UIMutableUserNotificationCategory()
-        registeredActions.identifier = "INVITE_Category"
-        registeredActions.setActions([acceptAction, trashAction, replyAction], forContext: UIUserNotificationActionContext.Default)
-        registeredActions.setActions([acceptAction, replyAction], forContext: UIUserNotificationActionContext.Minimal)
-        
-        
-        //Unicode scalars - I need to see if they are of the icomoon font pack.
-        
-        
-        //Base notification:
+    func setupNotifications() {
+        //Notification Actions:
         let checkAction = UIMutableUserNotificationAction()
         checkAction.title = "\u{e606}"
-        checkAction.identifier = "FINISH_ID"
+        checkAction.identifier = kNotification.Action.Check.toRaw()
         checkAction.activationMode = UIUserNotificationActivationMode.Background
         
         let cancelAction = UIMutableUserNotificationAction()
         cancelAction.title = "\u{e604}"
-        cancelAction.identifier = "CANCEL_ID"
+        cancelAction.identifier = kNotification.Action.Cancel.toRaw()
         cancelAction.destructive = true
         cancelAction.activationMode = UIUserNotificationActivationMode.Background
         
-        let happyAction = UIMutableUserNotificationAction()
-        happyAction.title = "\u{e602}"
-        happyAction.identifier = "HAPPY_ID"
-        happyAction.activationMode = UIUserNotificationActivationMode.Background
+        let rateUp = UIMutableUserNotificationAction()
+        rateUp.title = "\u{e602}"
+        rateUp.identifier = kNotification.Action.RateUp.toRaw()
+        rateUp.activationMode = UIUserNotificationActivationMode.Background
         
-        let madAction = UIMutableUserNotificationAction()
-        madAction.title = "\u{e603}"
-        madAction.identifier = "MAD_ID"
-        madAction.activationMode = UIUserNotificationActivationMode.Background
+        let rateDown = UIMutableUserNotificationAction()
+        rateDown.title = "\u{e603}"
+        rateDown.identifier = kNotification.Action.RateDown.toRaw()
+        rateDown.activationMode = UIUserNotificationActivationMode.Background
         
-        var pubCrawlCategory = UIMutableUserNotificationCategory()
-        pubCrawlCategory.identifier = "PUB_Category"
-        pubCrawlCategory.setActions([checkAction, cancelAction], forContext: UIUserNotificationActionContext.Minimal)
-        pubCrawlCategory.setActions([checkAction, happyAction, madAction, cancelAction], forContext: UIUserNotificationActionContext.Default)
-        
-        
-        //Uber notification:
-        var callAction = UIMutableUserNotificationAction()
+        let callAction = UIMutableUserNotificationAction()
         callAction.title = "\u{600} Uber"
-        callAction.identifier = "UBER_CALL_ID"
+        callAction.identifier = kNotification.Action.CallUber.toRaw()
         callAction.activationMode = UIUserNotificationActivationMode.Background
         
+        //The two notification categories used by the app.
+        let pubCrawlCategory = UIMutableUserNotificationCategory()
+        pubCrawlCategory.identifier = kNotification.Category.PubCrawl.toRaw()
+        pubCrawlCategory.setActions([checkAction, cancelAction], forContext: UIUserNotificationActionContext.Minimal)
+        pubCrawlCategory.setActions([checkAction, rateUp, rateDown, cancelAction], forContext: UIUserNotificationActionContext.Default)
         
-        var finalDestinationNotification = UIMutableUserNotificationCategory()
-        finalDestinationNotification.identifier = "UBER_Category"
-        finalDestinationNotification.setActions([callAction, cancelAction], forContext: UIUserNotificationActionContext.Default)
-        finalDestinationNotification.setActions([callAction, cancelAction], forContext: UIUserNotificationActionContext.Minimal)
+        let uberNotification = UIMutableUserNotificationCategory()
+        uberNotification.identifier = kNotification.Category.Uber.toRaw()
+        uberNotification.setActions([callAction, cancelAction], forContext: UIUserNotificationActionContext.Default)
+        uberNotification.setActions([callAction, cancelAction], forContext: UIUserNotificationActionContext.Minimal)
         
         
-        //Finesse a bit of logic to help the use not spend money on getting to a loction that they can easily walk to. We'll have to figure out a good place to get that working.
-        
-        
-        //This should be the same no matter what:
-        
-        //
+        //Takes defined category types and registers them within the application.
         var types = UIUserNotificationType.Alert | UIUserNotificationType.Sound
-        //
-        
-        //TODO: Stick UBER and the normal one in here. The registered sample one is perfect for prelim testing.
-        var categories = NSSet(objects: registeredActions)
+        var categories = NSSet(objects: pubCrawlCategory, uberNotification)
         
         var settings = UIUserNotificationSettings(forTypes: types, categories: categories)
         UIApplication.sharedApplication().registerUserNotificationSettings(settings)
         
-        /*
-        Handle with:
-        optional func application(_ application: UIApplication,
-        handleActionWithIdentifier identifier: String?,
-        forLocalNotification notification: UILocalNotification,
-        completionHandler completionHandler: () -> Void)
-        
-        Within this we will need to use : [self handleAcceptActionWithNotification:notification];
-        
-        */
-        
         
     }
+    
     
     //I'm keeping this here is people are interested in finding their custom font names later on. In their projects.
     func lookAtFontFamilies() {
