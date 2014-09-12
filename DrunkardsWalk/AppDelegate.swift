cda1c4aca29f46ce61dc406f58214d007658a05f
Leonardo Lee <lee.leonardo.king@gmail.com>
2014-09-11 19:37:58 -0700

    Implemented the basics for the background thread dispatch queues. The rest I'll deal with later.

--- DrunkardsWalk/AppDelegate.swift
+++ DrunkardsWalk/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var alert : UIAlertController?
 
 //MARK: -
+    //func applicationWillEnterForeground(application: UIApplication) {}
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
         // Override point for customization after application launch.
         
@@ -50,7 +51,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
-
+    
+    //func applicationDidBecomeActive(application: UIApplication) {}
     func applicationWillResignActive(application: UIApplication!) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -59,6 +61,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication!) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        //var backgroundTask = application.beginBackgroundTaskWithExpirationHandler { () -> Void in
+            //This is for tasks
+        //}
+        
+        //dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), { () -> Void in
+            //Handle task
+            //application.endBackgroundTask(backgroundTask)
+            //backgroundTask = UIBackgroundTaskInvalid
+        //})
+        
     }
 
     func applicationWillEnterForeground(application: UIApplication!) {
@@ -106,7 +119,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             askChange.addAction(change)
             askChange.addAction(okay)
             
-            self.window?.rootViewController?.presentViewController(askChange, animated: true, completion: nil)
+            //self.window?.rootViewController?.presentViewController(askChange, animated: true, completion: nil)
         }
     }
     
@@ -136,14 +149,52 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 switch action {
                 case .Check:
                     println()
+                    //var backgroundTask = application.beginBackgroundTaskWithExpirationHandler { () -> Void in
+                    //This is for tasks
+                    //}
+                    //dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), { () -> Void in
+                    //Handle task
+                    //application.endBackgroundTask(backgroundTask)
+                    //backgroundTask = UIBackgroundTaskInvalid
+                    //})
+                    
                 case .Cancel:
                     application.cancelLocalNotification(notification)
                 case .RateUp:
                     println()
+                    //var backgroundTask = application.beginBackgroundTaskWithExpirationHandler { () -> Void in
+                    //This is for tasks
+                    //}
+                    
+                    //dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), { () -> Void in
+                    //Handle task
+                    //application.endBackgroundTask(backgroundTask)
+                    //backgroundTask = UIBackgroundTaskInvalid
+                    //})
+                    
                 case .RateDown:
                     println()
+                    //var backgroundTask = application.beginBackgroundTaskWithExpirationHandler { () -> Void in
+                    //This is for tasks
+                    //}
+                    
+                    //dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), { () -> Void in
+                    //Handle task
+                    //application.endBackgroundTask(backgroundTask)
+                    //backgroundTask = UIBackgroundTaskInvalid
+                    //})
+                    
                 case .CallUber:
                     println()
+                    //var backgroundTask = application.beginBackgroundTaskWithExpirationHandler { () -> Void in
+                    //This is for tasks
+                    //}
+                    
+                    //dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), { () -> Void in
+                    //Handle task
+                    //application.endBackgroundTask(backgroundTask)
+                    //backgroundTask = UIBackgroundTaskInvalid
+                    //})
                 }
             }
         }
